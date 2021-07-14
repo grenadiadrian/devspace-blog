a8fc415ab62af79c03ee5b65e6a27a1539adbb3a
Adrian Mason <adrianlmason@gmail.com>
2021-07-14 11:43:57 -0500

    Update useState()

--- components/Search.js
+++ components/Search.js
@@ -3,7 +3,7 @@ import { FaSearch } from 'react-icons/fa'
 
 export default function Search() {
   const [searchTerm, setSearchTerm] = useState('')
-  const [searchResults, setSearchResults] = useState([''])
+  const [searchResults, setSearchResults] = useState([])
 
   return (
     <div className='relative bg-gray-600 p-4'>
